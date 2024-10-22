f98fbeb95795f3b2921da0634dcc6ef78af96815
Sezimar Jr <sezimar_jrcs@hotmail.com>
2024-10-22 18:39:23 -0300

    Componetizando o Hero

---
+++ src/components/Section/index.tsx
@@ -0,0 +1,19 @@
+import { Container, Title } from './styles'
+
+export type Props = {
+  title: string
+  background?: 'gray' | 'black'
+  children: JSX.Element
+}
+const Section = ({ title, background, children }: Props) => {
+  return (
+    <Container background={background}>
+      <div className="container">
+        <Title>{title}</Title>
+        {children}
+      </div>
+    </Container>
+  )
+}
+
+export default Section

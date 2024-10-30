bacf4d7349a24b64acf01ec9cb16d8e051eb63d8
Sezimar Jr <sezimar_jrcs@hotmail.com>
2024-10-30 18:33:37 -0300

    integrando com api de billing

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -8,10 +8,11 @@ import barCode from '../../assets/images/bar_code.png'
 import creditCard from '../../assets/images/credit_card.png'
 import { useFormik } from 'formik'
 import * as Yup from 'yup'
+import { usePurchaseMutation } from '../../services/api'
 
 const Checkout = () => {
   const [payWithCard, setPayWithCard] = useState(false)
-
+  const [purchase, { isLoading, isError, data }] = usePurchaseMutation()
   const form = useFormik({
     initialValues: {
       fullname: '',
@@ -72,7 +73,39 @@ const Checkout = () => {
       )
     }),
     onSubmit: (values) => {
-      console.log(values)
+      purchase({
+        billing: {
+          document: values.cpf,
+          email: values.email,
+          name: values.fullname
+        },
+        delivery: {
+          email: values.deliveryEmail
+        },
+        payment: {
+          installments: 1,
+          card: {
+            active: payWithCard,
+            code: Number(values.cardCode),
+            name: values.cardDisplayName,
+            number: values.cardNumber,
+            owner: {
+              document: values.cpfCardOwner,
+              name: values.cardOwner
+            },
+            expires: {
+              month: 1,
+              year: 2024
+            }
+          }
+        },
+        products: [
+          {
+            id: 1,
+            price: 10
+          }
+        ]
+      })
     }
   })
 
